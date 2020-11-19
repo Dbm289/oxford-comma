f92be454801f1f04bc7dac08c0fa8f218c03f323
Dan <DanielMooreRealPerson@gmail.com>
2020-11-18 22:04:18 -0500

    commit tree fc5d43b9ba57effe762e0ba9d236cda5978f905c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
+    if array.count > 2
+        last_element = array.pop
+        new_array = array.join( ", ")
+        new_array<<(", and")
+        new_array<<" #{last_element}"
+
+
+
+    
+    elsif array.count == 2
+        array.join(" and ")
+    else 
+        array.join 
+        
+    end
+
+
+
+    
+
+    
 
 end
